cf292e86eb28ca1f8c59b5c8b6d270c80dd306eb
Seyi Agboola <learnseyi@gmail.com>
2021-04-20 14:09:34 -0500

    update file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,14 @@
 import Main from '../Main/Main';
-import {BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 
 const App= ()=> {
   return (
     // importing browser router
-    <Router>
+    
        <div >
     <Main/>
     </div>
-    </Router>
+    
    
   );
 }
